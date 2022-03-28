e7b9f2274d9688f8d096af088ee9c3d2fc3d1341
Higo Henrique da Silva <silvahigo2@gmail.com>
2022-03-28 16:10:29 -0300

    Atividade Array

--- index.js
+++ index.js
@@ -2,70 +2,70 @@
 const input = document.getElementById("input");
 const input2 = document.getElementById("input-2");
 //Botões
-const buttonFim = document.getElementById("btn");
-const buttonInicio = document.getElementById("btn-2");
-const buttonLimpar = document.getElementById("btn-3");
-const exibirLista = document.getElementById("btn-5");
-const deletarIndice = document.getElementById("btn-4");
-const lista = document.getElementById("lista");
+const buttonEnd = document.getElementById("btn");
+const buttonStart = document.getElementById("btn-2");
+const buttonClean = document.getElementById("btn-3");
+const showList = document.getElementById("btn-5");
+const deleteIndex = document.getElementById("btn-4");
+const list = document.getElementById("lista");
 
-let vetor = [];
+let array = [];
 
-function inserirInicio(){
-    let valor = parseInt(input.value);
-    if(valor){
-        vetor.unshift(valor);
+function InsertStart(){
+    let value = parseInt(input.value);
+    if(value){
+        array.unshift(value);
         input.value = "";
         input.focus();
-        Lista();
+        List();
         
     }else{
         alert("Digite um Número!")
     }
 }
 
-function inserirFim(){
-    let valor = parseInt(input.value);
-    if(valor){ 
-        vetor.push(valor);
+function InsertEnd(){
+    let value = parseInt(input.value);
+    if(value){ 
+        array.push(value);
         input.value = "";
         input.focus()
-        Lista();
+        List();
     }else{
         alert("Digite um Número!")
     }
 }
 
-function Limpar(){
-    vetor = [];
+function Clean(){
+    array = [];
     input.value = "";
-    Lista();
+    List();
 }
 
-function Lista(){
-    lista.innerText = '';
-    vetor.forEach(n=>{
+function List(){
+    list.innerText = '';
+    array.forEach(n=>{
         item = document.createElement('li')
         item.textContent = n
-        lista.appendChild(item)
+        list.appendChild(item)
       })
 }
 
-function RemoverIndice(){
-    let valorIndice = parseInt(input2.value);
+function DeleteIndex(){
+    let valueIndex = parseInt(input2.value);
 
-    if(vetor[valorIndice]){
-        vetor.splice(valorIndice,1);
+    if(array[valueIndex]){
+        array.splice(valueIndex,1);
         input2.value = "";
         input2.focus();
-        Lista();
+        List();
     }else{
         alert("Digite um Indice válido");
         input2.value = "";
     }
 }
 
-buttonFim.onclick = inserirFim;
-buttonInicio.onclick = inserirInicio;
-buttonLimpar.onclick = Limpar;
-deletarIndice.onclick = RemoverIndice;
+buttonEnd.onclick = InsertEnd;
+buttonStart.onclick = InsertStart;
+buttonClean.onclick = Clean;
+deleteIndex.onclick = DeleteIndex;
